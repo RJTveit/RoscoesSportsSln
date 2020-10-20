9cc6613e9f1cf520a7cf6222eb7077265ed4fb81
Roscoe T <roscoe.tveit@gmail.com>
2020-10-20 09:51:09 -0700

    Implemented authorization and authentication, setup for docker deployment.

--- OutdoorProducts/Startup.cs
+++ OutdoorProducts/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using OutdoorProducts.Models;
+using Microsoft.AspNetCore.Identity;
 
 
 namespace OutdoorProducts
@@ -47,10 +48,27 @@ namespace OutdoorProducts
 
             services.AddServerSideBlazor();
 
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                 options.UseSqlServer(
+                    Configuration["ConnectionStrings:IdentityConnection"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppIdentityDbContext>();
+
         }
                 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/error");
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+
             app.UseDeveloperExceptionPage();
 
             app.UseStatusCodePages();
@@ -60,6 +78,10 @@ namespace OutdoorProducts
             app.UseSession();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
@@ -81,6 +103,7 @@ namespace OutdoorProducts
             });
 
             SeedData.EnsurePopulated(app);
+            IdentitySeedData.EnsurePopulated(app);
 
         }
     }
